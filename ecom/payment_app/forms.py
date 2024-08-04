ddfa2279c299b37e433915ba574baa20b6fbb82f
Rajdeep Bhattacharya <161099290+localgroup@users.noreply.github.com>
2024-08-04 21:27:42 +0530

    Updated processing order and related changes.

--- ecom/payment_app/forms.py
+++ ecom/payment_app/forms.py
@@ -4,7 +4,7 @@ from . models import ShippingAddress
 
 class ShippingForm(forms.ModelForm):
     shipping_full_name = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Full Name'}), required=True)
-    shipping_email = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}), required=True)
+    shipping_phone = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone Number'}), required=True)
     shipping_address1 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 1'}), required=True)
     shipping_address2 = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address 2'}), required=False)
     shipping_city = forms.CharField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), required=True)
@@ -14,7 +14,7 @@ class ShippingForm(forms.ModelForm):
     
     class Meta:
         model = ShippingAddress
-        fields = ['shipping_full_name', 'shipping_email', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
+        fields = ['shipping_full_name', 'shipping_phone', 'shipping_address1', 'shipping_address2', 'shipping_city', 'shipping_state', 'shipping_zipcode', 'shipping_country']
         
         exclude = ['user',]
         
--- ecom/payment_app/views.py
+++ ecom/payment_app/views.py
@@ -1,13 +1,101 @@
 from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment_app.models import ShippingAddress
+from payment_app.models import ShippingAddress, Order, OrderItem
 from payment_app.forms import ShippingForm, PaymentForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+from store.models import Product
 
 
 
 def process_order(request):
-    return render(request, "payment_app/process_order.html", {})
+    if request.POST:
+        # Get cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        
+        # Get billing info from the last page
+        payment_form = PaymentForm(request.POST or None)
+        
+        # Get shipping session data
+        my_shipping = request.session.get('my_shipping')
+        
+        #Get Order Info...
+        full_name = my_shipping['shipping_full_name']
+        phone_number = my_shipping['shipping_phone']
+        # Create Shipping Addesss from session info
+        shipping_address = (
+            f"{my_shipping['shipping_address1']}\n"
+            f"{my_shipping['shipping_address2']}\n"
+            f"{my_shipping['shipping_city']}, {my_shipping['shipping_state']} {my_shipping['shipping_zipcode']}\n"
+            f"{my_shipping['shipping_country']}\n"
+        )
+        amount_paid = totals
+        
+        # Create an order
+        if request.user.is_authenticated:
+            user = request.user
+            create_order = Order(user=user, full_name=full_name, phone=phone_number, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            
+            # Add order items...
+            # Get the order id
+            order_id = create_order.pk
+            
+            # Get product info
+            for product in cart_products():
+                product_id = product.id
+                
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                    
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        #Create order items
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user_id=user.id, quantity=value, price=price)
+                        create_order_item.save()
+            
+            
+            messages.success(request, "Order placed!")
+            return redirect('home')
+            
+        else:
+            create_order = Order(full_name=full_name, phone=phone_number, shipping_address=shipping_address, amount_paid=amount_paid)
+            create_order.save()
+            
+            # Add order items...
+            # Get the order id
+            order_id = create_order.pk
+            
+            # Get product info
+            for product in cart_products():
+                product_id = product.id
+                
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+                    
+                # Get quantity
+                for key, value in quantities().items():
+                    if int(key) == product.id:
+                        #Create order items
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+            
+            
+            messages.success(request, "Order placed!")
+            return redirect('home')
+    
+    else:
+        messages.success(request, "Access denied!")
+        return redirect('home')
+    # return render(request, "payment_app/process_order.html", {})
 
 
 def billing_info(request):
@@ -17,6 +105,10 @@ def billing_info(request):
         cart_products = cart.get_prods
         quantities = cart.get_quants
         totals = cart.cart_total()
+        
+        # Create a session with shipping info
+        my_shipping = request.POST
+        request.session['my_shipping'] = my_shipping
         
         # User Authentication
         if request.user.is_authenticated:
