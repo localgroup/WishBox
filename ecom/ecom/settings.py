c004954ae2174a4b5eaecec929379617d4a245f2
Rajdeep Bhattacharya <161099290+localgroup@users.noreply.github.com>
2024-08-10 20:56:42 +0530

    Migrate database to railway-postgres
    Migrate the db from SQLLite to PostgreSQL hosted in railway. Add minor adjustments.

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'railway',
-        # 'USER': 'postgres',
-        # 'PASSWORD': DB_PASSWORD,
-        # 'HOST': 'postgres.railway.internal',
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': DB_PASSWORD,
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '54063',
     }
 }
 
